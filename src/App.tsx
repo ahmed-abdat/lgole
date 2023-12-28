f986ade3d09363ccd0716c87ff8856db3b67e1cf
= <ahmedeabdat@gmail.com>
2023-12-28 22:44:18 +0000

    Remove unused imports and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 
-import Header from "./layout/Header";
+
 import { ThemeProvider } from "./components/theme-provider";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import { lazy, Suspense } from "react";
 import Home from "./pages/home";
-import Footer from "./pages/Footer";
 import "./App.css";
 import NoteFound from "./pages/noteFound";
 
--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,11 +1,8 @@
 import * as React from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import FolderIcon from "@mui/icons-material/Folder";
-import RestoreIcon from "@mui/icons-material/Restore";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
+
 import {
-  CubeIcon,
   GearIcon,
   HomeIcon,
   QuestionMarkCircledIcon,
