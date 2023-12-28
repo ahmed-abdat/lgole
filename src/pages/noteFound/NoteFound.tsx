823869346b3df8a4ba4bd985bb39bf979b5399f1
= <ahmedeabdat@gmail.com>
2023-12-28 22:50:55 +0000

    Add custom drawer component to NoteFound page

--- src/pages/noteFound/NoteFound.tsx
+++ src/pages/noteFound/NoteFound.tsx
@@ -1,10 +1,12 @@
 import Footer from "../Footer";
+import CustomeDrawer from "@/layout/Drawer";
 
 
 function NoteFound() {
   return (
     <div className="flex items-center justify-center p-4 ">
       <h1 className="mt-56">Page not found</h1>
+      <CustomeDrawer />
         <Footer />
     </div>
   );
